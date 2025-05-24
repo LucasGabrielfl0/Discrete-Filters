76cb8582d9c842e2504f4ca217075f6ec801c1b9
Gabriel <119896525+LucasGabrielfl0@users.noreply.github.com>
2025-05-24 15:46:55 -0300

    Update RecursiveFilters.c

--- RecursiveFilters.c
+++ RecursiveFilters.c
@@ -1,1 +1,11 @@
 // Recursive Average
+int X_avg[2];
+X_avg[0] = 0;   // Current value for the average
+X_avg[1] = 0;   // Previous value for the average;
+
+int Xk = 0;    // Value that changes in discrete steps of K
+int k = 0;    //  Number os iterations
+
+// Average Filter is X_avg = (k-1/k)*X_avg[1] +(1/k)*Xk , or:
+int alpha = (k=1)/k
+int X_avg = alpha*X_avg[1] +(1-alpha)*Xk;
